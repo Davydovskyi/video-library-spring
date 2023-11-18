7e5d3eba41aeadfbd86aeac7c4979fdc2fd39a35
Anton Davydovskyi <anton.davydoone@mail.ru>
2023-11-18 17:58:45 +0300

    feature-Security-functionality

---
+++ src/main/java/edu/jcourse/config/OpenApiConfiguration.java
@@ -0,0 +1,21 @@
+package edu.jcourse.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "oauth2",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "http://localhost:8080/login/oauth2/code/google",
+                        tokenUrl = "https://www.googleapis.com/oauth2/v4/token"
+                )
+        )
+)
+public class OpenApiConfiguration {
+}
--- src/main/java/edu/jcourse/config/SecurityConfiguration.java
+++ src/main/java/edu/jcourse/config/SecurityConfiguration.java
@@ -1,33 +1,106 @@
 package edu.jcourse.config;
 
+import edu.jcourse.database.entity.Role;
+import edu.jcourse.dto.user.UserCreateEditDto;
+import edu.jcourse.mapper.user.UserCreateEditMapper;
+import edu.jcourse.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AnonymousConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Collections;
+import java.util.Set;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final UserService userService;
+    private final UserCreateEditMapper userCreateEditMapper;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .anonymous(AnonymousConfigurer::disable)
                 .authorizeHttpRequests(urlConfig -> urlConfig
                         .requestMatchers(
                                 antMatcher("/login"), antMatcher("/registration"),
                                 antMatcher("/v3/api-docs/**"), antMatcher("/swagger-ui/**"),
-                                antMatcher(HttpMethod.POST, "/users"),
-                                antMatcher(HttpMethod.POST, "/api/v1/users")).permitAll()
-                        .anyRequest().permitAll())
+                                antMatcher(HttpMethod.POST, "/users")).permitAll()
+                        .requestMatchers(
+                                antMatcher("/users/{\\d+}/delete"), antMatcher("/users/{\\d+}/update"),
+                                antMatcher("/movie-persons/add/**"), antMatcher(HttpMethod.POST, "/users"),
+                                antMatcher("/movies/add"), antMatcher(HttpMethod.POST, "movies"),
+                                antMatcher("/movies/{\\d+}/pre-update"), antMatcher("/movies/{\\d+}/delete"),
+                                antMatcher("/movies/{\\d+}/update"), antMatcher("/persons/add"),
+                                antMatcher(HttpMethod.POST, "/persons"),
+                                antMatcher("/persons/{\\d+}/delete"), antMatcher("/persons/{\\d+}/update")).hasAuthority(Role.ADMIN.getAuthority())
+                        .anyRequest().authenticated())
                 .formLogin(login -> login.loginPage("/login")
                         .defaultSuccessUrl("/movies"))
                 .logout(logout -> logout.logoutUrl("/logout")
                         .logoutSuccessUrl("/login")
-                        .deleteCookies("JSESSIONID"));
+                        .deleteCookies("JSESSIONID"))
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/movies")
+                        .userInfoEndpoint(userInfo ->
+                                userInfo.oidcUserService(oidcUserService())));
         return http.build();
+    }
+
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return request -> {
+            String email = request.getIdToken().getEmail();
+            UserDetails maybeUserDetails;
+            try {
+                maybeUserDetails = userService.loadUserByUsername(email);
+            } catch (UsernameNotFoundException e) {
+                UserCreateEditDto createEditDto = createUser(request.getIdToken());
+                userService.create(createEditDto);
+                edu.jcourse.database.entity.User user = userCreateEditMapper.map(createEditDto);
+                maybeUserDetails = new User(user.getEmail(), user.getPassword(), Collections.singleton(user.getRole()));
+            }
+
+            UserDetails userDetails = maybeUserDetails;
+
+            DefaultOidcUser oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), request.getIdToken());
+
+            Set<Method> userDetailsMethods = Set.of(UserDetails.class.getMethods());
+
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{OidcUser.class, UserDetails.class},
+                    (proxy, method, args) ->
+                            userDetailsMethods.contains(method)
+                                    ? method.invoke(userDetails, args)
+                                    : method.invoke(oidcUser, args));
+        };
+    }
+
+    private UserCreateEditDto createUser(OidcIdToken idToken) {
+        return UserCreateEditDto.builder()
+                .email(idToken.getEmail())
+                .username(idToken.getFullName())
+                .role(Role.USER)
+                .rawPassword(idToken.getAccessTokenHash())
+                .build();
     }
 }
